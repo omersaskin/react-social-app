898c304d31fc6e780c019c61a79f5688153dd120
Ömer Şaşkın <pc.omer.saskin@gmail.com>
2023-09-05 12:34:02 +0300

    Fake veri güncellendi

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -25,7 +25,7 @@ export default function Profile() {
               />
             </div>
             <div className="profileInfo">
-                <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                <h4 className="profileInfoName">Lia Pole</h4>
                 <span className="profileInfoDesc">Hello my friends!</span>
             </div>
           </div>

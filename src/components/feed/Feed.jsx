9a224ff05c72876deee51aac201361cb777530fd
Ömer Şaşkın <pc.omer.saskin@gmail.com>
2023-09-05 09:36:41 +0300

    Share & Post komponentlerinin eklenmesi

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,18 @@
+import Share from '../share/Share'
+import Post from '../post/Post'
 import './feed.css'
 
 export default function Feed() {
   return (
     <div className='feed'>
-      Feed
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   )
 }
